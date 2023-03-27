217c70f049963dd68cdf9c68f2e89eb319f449a9
kousha ghodsizad <kousha.ghodsizad@gmail.com>
2023-03-27 02:09:39 +0200

    change connection to ws

--- background.js
+++ background.js
@@ -1,35 +1,73 @@
 console.log("Cheater Background is loaded");
-async function sendSelectedTextToServer(text) {
-  const url = 'http://localhost:8080/push';
-  const data = { text: text };
 
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
+const apiKey = "test";
+// Define the WebSocket URL
+const serverURL = "wss://cheater-server-mbmu9.ondigitalocean.app/ws";
+
+// Create a WebSocket connection
+const socket = new WebSocket(serverURL);
+
+async function sendTextToWS(text) {
+    // Connection opened
+    socket.addEventListener("open", (event) => {
+        // Check if the WebSocket connection is open before sending the message
+        if (socket.readyState === WebSocket.OPEN) {
+            // Create the JSON structure
+            const message = {
+                apiKey: apiKey,
+                client: "mac",
+                content: text,
+            };
+
+            // Send the JSON structure as a string
+            socket.send(JSON.stringify(message));
+        } else {
+            setTimeout(() => {
+                connectToWS();
+                sendTextToWS(text);
+            }, 1000);
+        }
     });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error ${response.status}`);
-    }
-
-    console.log('Selected text sent successfully');
-  } catch (error) {
-    console.error('Failed to send selected text:', error);
-  }
 }
 
+
+async function connectToWS() {
+// Connection opened
+    socket.addEventListener("open", (event) => {
+        // JSON content to be sent
+        const message = {
+            apiKey: apiKey,
+            client: "firefox",
+            content: "register",
+        };
+
+        // Send the JSON content as a string
+        socket.send(JSON.stringify(message));
+    });
+}
+
+// Connection closed
+socket.addEventListener("close", (event) => {
+    connectToWS();
+    console.log("WebSocket connection closed:", event);
+});
+
+// Connection error
+socket.addEventListener("error", (event) => {
+    connectToWS();
+    console.log("WebSocket connection closed:", event);
+});
+
 browser.browserAction.onClicked.addListener(() => {
-  browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    browser.tabs.sendMessage(tabs[0].id, 'cheatSelectedText');
-  });
+    browser.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        browser.tabs.sendMessage(tabs[0].id, 'cheatSelectedText');
+    });
 });
 
 browser.runtime.onMessage.addListener((message) => {
-  if (message.action === 'sendTextToServer') {
-    sendSelectedTextToServer(message.text);
-  }
+    if (message.action === 'sendTextToServer') {
+        sendSelectedTextToServer(message.text);
+    }
 });
+
+connectToWS();
